1c458ca4e366ab5da9c932eb13d1505a2ef48923
Rosemberg-Diaz <63666604+Rosemberg-Diaz@users.noreply.github.com>
2023-09-25 17:22:46 -0500

    Update settings.py

--- Dikkat/settings.py
+++ Dikkat/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/Static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'Static'),)
 
 MEDIA_URL = '/Media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
