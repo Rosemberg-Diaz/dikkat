e806c99710795fd209e270341b0f56cc7292e24e
Rosemberg-Diaz <63666604+Rosemberg-Diaz@users.noreply.github.com>
2023-09-25 18:35:00 -0500

    Update settings.py

--- Dikkat/settings.py
+++ Dikkat/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-5c!si5(v9nvh5t4#4m2gcp1q=yd$vz1r0l^=uq&z2^=iku)@+n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
