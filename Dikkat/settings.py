0473f729a574da9f9d4ab110ea9a631e6ab90e19
Rosemberg-Diaz <63666604+Rosemberg-Diaz@users.noreply.github.com>
2023-09-27 21:24:24 -0500

    Update settings.py

--- Dikkat/settings.py
+++ Dikkat/settings.py
@@ -116,11 +116,11 @@ LOGIN_URL = reverse_lazy('inicio')
 
 # Email Settings (configure in Heroku environment variables)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ.get('EMAIL_HOST')
-EMAIL_PORT = os.environ.get('EMAIL_PORT')
-EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'False').lower() == 'true'
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'dikkatrc@gmail.com'
+EMAIL_HOST_PASSWORD = 'hvpw tpxy fsnq dlbh'
 
 
 
